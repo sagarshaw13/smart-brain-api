9e45e8c77eb4c857f1de8ee67e7f6e2358fc6792
sagar_2001 <sagarshaw2001@gmail.com>
2021-04-16 23:05:37 +0530

    added port

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/imageURL',(req, res) => {image.hangleImageApi(req, res)})
 
-app.listen(3000, ()=>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
 
