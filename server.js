6eb65e04ccb05cdc998dff849a667e4a1d1bf097
sagar_2001 <sagarshaw2001@gmail.com>
2021-04-16 23:33:34 +0530

    updates

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/imageURL',(req, res) => {image.hangleImageApi(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log(`app is running on port ${process.env.PORT}`);
 });
 
